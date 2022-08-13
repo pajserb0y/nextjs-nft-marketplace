67ddf4a167c38665d7a0bb56daebc2c8ea6190ae
pajserb0y <65039799+pajserb0y@users.noreply.github.com>
2022-08-13 18:55:33 +0200

    home page init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
-
+import "../styles/globals.css"
+import Head from "next/head"
+import { MoralisProvider } from "react-moralis"
+import Header from "../components/Header"
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        //initializeOnMount is used for moralis Server which we are going to use later and then it willbe true
+        <div>
+            <Head>
+                <title>NFT Marketplace</title>
+                <meta name="description" content="NFT Marketplace" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <MoralisProvider initializeOnMount={false}>
+                <Header />
+                <Component {...pageProps} />
+            </MoralisProvider>
+        </div>
+    )
 }
 
 export default MyApp
